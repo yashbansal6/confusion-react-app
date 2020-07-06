5981f231b712320c9b80ded625d491c1a3ae8f7b
Yash Bansal <yashbansal@outlook.com>
2020-07-06 18:14:54 +0530

    single page appn: v2

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Card, CardImg, CardBody, CardText, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardBody, CardText, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
     function RenderComments({comments}) {
         if (comments == null) {
@@ -47,13 +48,22 @@ import { Card, CardImg, CardBody, CardText, CardTitle } from 'reactstrap';
     }
 
     const Dishdetail = (props) => {
-        const dish = props.selectedDish;
-        if (dish != null) {
+        if (props.dish != null) {
             return (
                 <div className="container">
                     <div className="row">
-                        <RenderDish dish={dish} />
-                        <RenderComments comments={dish.comments} />
+                        <Breadcrumb>
+                            <BreadcrumbItem><Link to='/menu'>Menu</Link></BreadcrumbItem>
+                            <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                        </Breadcrumb>
+                        <div className="col-12">
+                            <h3>{props.dish.name}</h3>
+                            <hr />
+                        </div>
+                    </div>    
+                    <div className="row">
+                        <RenderDish dish={props.dish} />
+                        <RenderComments comments={props.comments} />
                     </div>
                 </div>
             );
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -28,8 +28,16 @@ class Main extends Component {
 		const HomePage = () => {
 			return (
 				<Home dish={this.state.dishes.filter((dish) => dish.featured)[0]}
-					  promotion={this.state.promotions.filter((promo) => promo.featured)[0]} 
-					  leader={this.state.leaders.filter((leader) => leader.featured)[0]} />
+					promotion={this.state.promotions.filter((promo) => promo.featured)[0]} 
+					leader={this.state.leaders.filter((leader) => leader.featured)[0]} />
+			);
+		}
+
+		const DishWithId = ({match}) => {
+			return (
+				<Dishdetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId, 10))[0]} 
+					comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId, 10))}
+				/>
 			);
 		}
 
@@ -39,6 +47,7 @@ class Main extends Component {
 				<Switch>
 					<Route path="/home" component={HomePage} />
 					<Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
+					<Route path="/menu/:dishId" component={DishWithId} />
 					<Route exact path="/contactus" component={Contact} />
 					<Redirect to="/home" />
 				</Switch>
